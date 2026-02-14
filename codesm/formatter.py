467b8d03e5b95e1fca95f415ad62f591ae468209
Madhurima <142151605+dorkdiaries9@users.noreply.github.com>
2026-02-14 22:36:16 +0530

    Add exception variable to formatter availability check

--- codesm/formatter.py
+++ codesm/formatter.py
@@ -158,7 +158,7 @@ class Formatter:
             available = proc.returncode == 0
             self._available_formatters[formatter] = available
             return available
-        except Exception:
+        except Exception as e :
             self._available_formatters[formatter] = False
             return False
     
